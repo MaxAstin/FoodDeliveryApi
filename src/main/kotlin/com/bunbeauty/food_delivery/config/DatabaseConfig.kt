35f160a7878ff77551a59f1d94aef73beddf784c
xIdealo <shavl.mark@yandex.ru>
2021-08-26 12:14:56 +0300

    start check heroku

--- src/main/kotlin/com/bunbeauty/food_delivery/config/DatabaseConfig.kt
+++ src/main/kotlin/com/bunbeauty/food_delivery/config/DatabaseConfig.kt
@@ -1,33 +1,23 @@
 package com.bunbeauty.food_delivery.config
 
+import com.zaxxer.hikari.HikariConfig
+import com.zaxxer.hikari.HikariDataSource
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import org.springframework.jdbc.datasource.DriverManagerDataSource
 import javax.sql.DataSource
+
 
 @Configuration
 class DatabaseConfig {
 
-@Value("\${spring.datasource.url}")
-    private lateinit var datasourceUrl: String
-
-    @Value("\${spring.datasource.driver-class-name}")
-    private lateinit var dbDriverClassName: String
-
-    @Value("\${spring.datasource.username}")
-    private lateinit var dbUsername: String
-
-    @Value("\${spring.datasource.password}")
-    private lateinit var dbPassword: String
+    @Value("\${spring.datasource.url}")
+    private var dbUrl: String? = null
 
     @Bean
-    fun dataSource(): DataSource =
-            DriverManagerDataSource().apply {
-                setDriverClassName(dbDriverClassName)
-                url = datasourceUrl
-                username = dbUsername
-                password = dbPassword
-            }
-
+    fun dataSource(): DataSource? {
+        val config = HikariConfig()
+        config.jdbcUrl = dbUrl
+        return HikariDataSource(config)
+    }
 }
