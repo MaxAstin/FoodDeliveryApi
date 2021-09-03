bef5d9ce0b97ad6fb3e27be6b1a0448d34101df3
xIdealo <shavl.mark@yandex.ru>
2021-09-03 15:18:38 +0300

    added data.sql

---
+++ src/main/resources/data.sql
@@ -0,0 +1,17 @@
+INSERT INTO cafe (uuid,
+                  from_time,
+                  to_time,
+                  phone,
+                  latitude,
+                  longitude,
+                  visible,
+                  address)
+VALUES ('cafe_uuid',
+        '10:00',
+        '22:00',
+        '89999999999',
+        56.0,
+        56.0,
+        true,
+        'kimry');
+
