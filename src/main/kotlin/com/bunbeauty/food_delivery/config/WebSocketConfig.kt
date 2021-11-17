0d0fb7f69669318e80f68f4c49cc75b1c493325d
maxastin <max.astin@yandex.ru>
2021-11-17 14:57:27 +0300

    update:websocket

--- src/main/kotlin/com/bunbeauty/food_delivery/config/WebSocketConfig.kt
+++ src/main/kotlin/com/bunbeauty/food_delivery/config/WebSocketConfig.kt
@@ -1,60 +1,36 @@
 package com.bunbeauty.food_delivery.config
 
+import com.bunbeauty.food_delivery.model.local.order.UserOrder
 import org.springframework.context.annotation.Configuration
 import org.springframework.web.socket.TextMessage
-import org.springframework.web.socket.WebSocketMessage
 import org.springframework.web.socket.WebSocketSession
-import org.springframework.web.socket.config.annotation.*
+import org.springframework.web.socket.config.annotation.EnableWebSocket
+import org.springframework.web.socket.config.annotation.WebSocketConfigurer
+import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry
 import org.springframework.web.socket.handler.TextWebSocketHandler
-import kotlin.concurrent.thread
 
 @Configuration
 @EnableWebSocket
 class WebSocketConfig : WebSocketConfigurer {
 
-//    override fun registerStompEndpoints(registry: StompEndpointRegistry) {
-//        registry.addEndpoint("/our-websoket").withSockJS()
-//    }
-//
-//    override fun configureMessageBroker(registry: MessageBrokerRegistry) {
-//        registry.enableSimpleBroker("/topic")
-//        registry.setApplicationDestinationPrefixes("/ws")
-//    }
-
     override fun registerWebSocketHandlers(registry: WebSocketHandlerRegistry) {
-        println("registerWebSocketHandlers")
-        registry.addHandler(OrderHandler(), "/chat")
+        registry.addHandler(OrderHandler(), "/order")
             .setAllowedOrigins("*")
-//            .setHandshakeHandler { request, response, wsHandler, attributes ->
-//                println("request $request \nresponse $response \nwsHandler $wsHandler \nattributes $attributes")
-//                true
-//            }
     }
 }
 
-class OrderHandler: TextWebSocketHandler() {
+class OrderHandler : TextWebSocketHandler() {
+
+    private val sessionMap: MutableMap<String, WebSocketSession> = HashMap()
 
     override fun afterConnectionEstablished(session: WebSocketSession) {
         super.afterConnectionEstablished(session)
 
-        Thread.sleep(5000)
-        session.sendMessage(TextMessage("connected " + System.nanoTime()))
-//        thread {
-//            Thread.sleep(5000)
-//            session.sendMessage(TextMessage("order " + System.nanoTime()))
-//        }.start()
+        val profileUuid = session.attributes["profileUuid"].toString()
+        sessionMap[profileUuid] = session
     }
 
-    override fun handleMessage(session: WebSocketSession, message: WebSocketMessage<*>) {
-
-        //Thread.sleep(5000)
-
-//        thread {
-//            Thread.sleep(5000)
-//            session.sendMessage(TextMessage("order " + System.nanoTime()))
-//        }.start()
-
-        Thread.sleep(5000)
-        session.sendMessage(TextMessage("order " + System.nanoTime()))
+    fun sendUpdatedOrder(order: UserOrder) {
+        sessionMap[order.profile.uuid]?.sendMessage(TextMessage(order.toString()))
     }
 }
